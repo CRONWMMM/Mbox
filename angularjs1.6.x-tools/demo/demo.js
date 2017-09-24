92114152704a74f1bf102014ef37eebfbe185251
CRONWMMM <kimmmiy@sina.cn>
2017-09-24 23:15:09 +0800

    添加了dialog指令，有待完善

--- angularjs1.6.x-tools/demo/demo.js
+++ angularjs1.6.x-tools/demo/demo.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 var app = angular.module('myApp',[]);
 
@@ -166,7 +166,9 @@ app.controller('selectCtrl',['$scope',function($scope){
 
 
 
-
+/**
+ * switch开关指令
+ */
 app.controller('switchCtrl',['$scope',function($scope){
 		$scope.title = 'Switch';
 		$scope.switch1 = false;
@@ -185,5 +187,83 @@ app.controller('switchCtrl',['$scope',function($scope){
 			}]
 		};
 	}]);
+
+
+
+app.controller('dragCtrl',['$scope',function($scope){
+	$scope.title = "Draggable-Dialog";
+	$scope.dialog = {
+		show : true,
+		showDialog : function(){
+			$scope.dialog.show = true;
+		}
+	};
+}]).directive('myDialog',['$document',function($document){
+	return {
+		restrict : "AE",
+		templateUrl : "./template/dialog.html",
+		scope : {
+			show : "=show"
+		},
+		transclude : true,
+		controller : ['$scope',function($scope){
+			$scope.closeDialog = function(){
+				$scope.show = false;
+			};
+		}],
+		link : function(scope,element,attr){
+			var header = element.find('div').eq(1),
+				startX,
+				startY,
+				x,
+				y,
+				clientStartX,
+				clientStartY;
+			header.on('mousedown',function($event){
+				var posObj =  _getPosition(element[0]);
+				startX = posObj.left;
+				startY = posObj.top;
+				clientStartX = $event.clientX;
+				clientStartY = $event.clientY;
+				$event.stopPropagation();
+				$event.preventDefault();
+				$document.on('mousemove',_mousemove);
+				$document.on('mouseup',_mouseup);
+			});
+
+			function _mousemove($event){
+				x = startX + $event.clientX - clientStartX;
+				y = startY + $event.clientY - clientStartY;
+				if(x<=0){
+					x = 0;
+				}
+				if(y<=0){
+					y = 0;
+				}
+				element.css({
+					left : x + 'px',
+					top : y + 'px',
+					bottom: 'auto',
+					right: 'auto',
+					margin: 0
+				});
+			}
+
+			function _mouseup(){
+				$document.off('mousemove');
+				$document.off('mouseup');
+			}
+
+			function _getPosition(obj){
+				return {
+					left : obj.offsetLeft,
+					top : obj.offsetTop
+				};
+			}
+		}
+	};
+}]);
+
+
 
 
